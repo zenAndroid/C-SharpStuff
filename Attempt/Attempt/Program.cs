ec8bbef612d1e8dff184641ed9beba11ae0a7e49
zenAndroid <21260658+zenAndroid@users.noreply.github.com>
2022-11-06 12:52:50 -0500

    Initial commit.

---
+++ Attempt/Attempt/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System;
+
+class HellWorld
+{
+    static double[] numbers;
+    static void Main()
+    {
+        Console.Write(string.Format("Check Check, {0} {1}, {0} {1:0.0}", 1, 2));
+        var list = new List<double>();
+    }
+}
---
+++ Chap01/TopLevel/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello from a Top Level Program!");
+Console.WriteLine(Environment.OSVersion.VersionString);
+int v = 5;
