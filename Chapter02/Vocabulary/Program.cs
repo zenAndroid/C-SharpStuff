0472f1717990f45842fb7738cca77f2724a8e233
zenAndroid <21260658+zenAndroid@users.noreply.github.com>
2022-11-06 22:36:44 -0500

    A taste of reflection; more to come.

--- Chapter02/Vocabulary/Program.cs
+++ Chapter02/Vocabulary/Program.cs
@@ -1,3 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // #error version
-Console.WriteLine("Hello, World!");
+using System.Reflection;
+
+Assembly? assembly = Assembly.GetEntryAssembly();
+if (assembly == null) return;
+// loop through the assemblies that this app references
+foreach (AssemblyName name in assembly.GetReferencedAssemblies())
+{
+    // load the assembly so we can read its details
+    Assembly a = Assembly.Load(name);
+    // declare a variable to count the number of methods
+    int methodCount = 0;
+    // loop through all the types in the assembly
+    foreach (TypeInfo t in a.DefinedTypes)
+    {
+        // add up the counts of methods
+        methodCount += t.GetMethods().Count();
+    }
+    // output the count of types and their methods
+    Console.WriteLine(
+    "{0:N0} types with {1:N0} methods in {2} assembly.",
+    arg0: a.DefinedTypes.Count(),
+    arg1: methodCount, arg2: name.Name);
+}
+
+/*
+The output is: 
+0 types with 0 methods in System.Runtime assembly.
+106 types with 1,126 methods in System.Linq assembly.
+44 types with 645 methods in System.Console assembly.
+*/
