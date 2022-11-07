7f478410209fadce8c989da714e66db6f2f81f8c
zenAndroid <21260658+zenAndroid@users.noreply.github.com>
2022-11-07 12:57:55 -0500

    Interesting.
    var keyword; dynamic vars; default values and how to find the default for a primitive; the ability to reset values to their default

--- Chapter02/Variables/Program.cs
+++ Chapter02/Variables/Program.cs
@@ -1,4 +1,7 @@
-﻿object height = 1.88; // storing a double in an object
+﻿using System.Diagnostics.Metrics;
+using System.Xml;
+    
+object height = 1.88; // storing a double in an object
 object name = "Amir"; // storing a string in an object
 Console.WriteLine($"{name} is {height} metres tall.");
 // int length1 = name.Length; // gives compile error!
@@ -15,3 +18,32 @@ something = 12;
 something = new[] { 3, 5, 7 };
 
 Console.WriteLine(something.Length);
+
+// good use of var because it avoids the repeated type
+// as shown in the more verbose second statement
+var xml1 = new XmlDocument();
+// bad use of var because we cannot tell the type, so we
+// should use a specific type declaration as shown in
+// the second statement
+
+Console.WriteLine($"default(int) = {default(int)}");
+Console.WriteLine($"default(bool) = {default(bool)}");
+Console.WriteLine($"default(DateTime) = {default(DateTime)}");
+Console.WriteLine($"default(string) = {default(string)}");
+
+int number = 13;
+Console.WriteLine($"number has been set to: {number}");
+number = default;
+Console.WriteLine($"number has been reset to its default: {number}");
+
+/*
+Amir is 1.88 metres tall.
+Amir has 4 characters.
+3
+default(int) = 0
+default(bool) = False
+default(DateTime) = 0001 - 01 - 01 12:00:00 AM
+default(string) =
+number has been set to: 13
+number has been reset to its default: 0
+*/
