cedf5c456fa6d3dd5106575d77bee8f07979855d
zenAndroid <21260658+zenAndroid@users.noreply.github.com>
2022-11-07 14:04:44 -0500

    Formatting

---
+++ Chapter02/Formatting/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int numberOfApples = 12;
+decimal pricePerApple = 0.35M;
+
+Console.WriteLine(
+    format: "{0} apples costs {1:C}",
+    arg0: numberOfApples,
+    arg1: pricePerApple * numberOfApples);
+
+string formatted = string.Format(
+    format: "{0} apples costs {1:C}",
+    arg0: numberOfApples,
+    arg1: pricePerApple * numberOfApples);
+
+//WriteToFile(formatted); // writes the string into a file
