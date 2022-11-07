be7e92da28ccd9c420eb6035dd90314399a5618f
zenAndroid <21260658+zenAndroid@users.noreply.github.com>
2022-11-07 14:23:06 -0500

    COMMAND LINE ARGUMENTS;
    EGADS

---
+++ Chapter02/Arguments/Program.cs
@@ -0,0 +1,19 @@
+﻿using static System.Console;
+
+WriteLine($"There are {args.Length} arguments.");
+
+// 0 args before fucking with args, 4 after passing in :firstarg second-arg third:arg "fourth arg":, WOW
+
+// i should stop fucking around, death approaches and i am wasting time
+foreach (string arg in args)
+{
+    WriteLine(arg);
+}
+
+/*
+There are 4 arguments.
+firstarg
+second-arg
+third:arg
+fourth arg
+*/
--- Chapter02/Formatting/Program.cs
+++ Chapter02/Formatting/Program.cs
@@ -46,3 +46,11 @@ Write("Type your age and press ENTER: ");
 string? age = ReadLine();
 WriteLine(
 $"Hello {firstName}, you look good for {age}.");
+
+Write("Press any key combination: ");
+ConsoleKeyInfo key = ReadKey();
+WriteLine();
+WriteLine("Key: {0}, Char: {1}, Modifiers: {2}",
+arg0: key.Key,
+arg1: key.KeyChar,
+arg2: key.Modifiers);
