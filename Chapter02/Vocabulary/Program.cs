8d517c6ed04bf3ce9600af21f7e21632cca2070c
zenAndroid <21260658+zenAndroid@users.noreply.github.com>
2022-11-07 01:04:40 -0500

    nameof keyword

--- Chapter02/Vocabulary/Program.cs
+++ Chapter02/Vocabulary/Program.cs
@@ -1,35 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // #error version
-using System.Reflection;
 
-Assembly? assembly = Assembly.GetEntryAssembly();
-if (assembly == null) return;
-// loop through the assemblies that this app references
-foreach (AssemblyName name in assembly.GetReferencedAssemblies())
-{
-    HttpClient client = new HttpClient();
-    System.Data.DataSet dataSet;
-    // load the assembly so we can read its details
-    Assembly a = Assembly.Load(name);
-    // declare a variable to count the number of methods
-    int methodCount = 0;
-    // loop through all the types in the assembly
-    foreach (TypeInfo t in a.DefinedTypes)
-    {
-        // add up the counts of methods
-        methodCount += t.GetMethods().Count();
-    }
-    // output the count of types and their methods
-    Console.WriteLine(
-    "{0:N0} types with {1:N0} methods in {2} assembly.",
-    arg0: a.DefinedTypes.Count(),
-    arg1: methodCount, arg2: name.Name);
-}
+double heightInMetres = 1.88;
+Console.WriteLine($"The variable {nameof(heightInMetres)} has the value {heightInMetres}.");
 
 /*
-0 types with 0 methods in System.Runtime assembly.
-458 types with 4,692 methods in System.Net.Http assembly.
-383 types with 6,854 methods in System.Data.Common assembly.
-106 types with 1,126 methods in System.Linq assembly.
-44 types with 645 methods in System.Console assembly.
+The variable heightInMetres has the value 1.88.
 */
