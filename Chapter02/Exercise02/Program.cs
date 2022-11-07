2f59b8c74df6f70646d39c1bcb849e63246f2898
zenAndroid <21260658+zenAndroid@users.noreply.github.com>
2022-11-07 15:59:03 -0500

    First attempt, so i got the format string right, but the formatting needs more work hmmm ...
    also wow visual studio is a piece os shit?

---
+++ Chapter02/Exercise02/Program.cs
@@ -0,0 +1,13 @@
+﻿using static System.Console;
+
+WriteLine("{0, -8} {1, 1} {2, 30} {3, 30}", "Type", "Byte(s) of memory", "Min", "Max");
+WriteLine("{0, -8} {1, 1} {2, 30} {3, 30}", "sbyte", sizeof(sbyte), sbyte.MinValue, sbyte.MaxValue);
+WriteLine("{0, -8} {1, 1} {2, 30} {3, 30}", "byte", sizeof(byte), byte.MinValue, byte.MaxValue);
+WriteLine("{0, -8} {1, 1} {2, 30} {3, 30}", "short", sizeof(short), short.MinValue, short.MaxValue);
+WriteLine("{0, -8} {1, 1} {2, 30} {3, 30}", "int", sizeof(int), int.MinValue, int.MaxValue);
+WriteLine("{0, -8} {1, 1} {2, 30} {3, 30}", "uint", sizeof(uint), uint.MinValue, uint.MaxValue);
+WriteLine("{0, -8} {1, 1} {2, 30} {3, 30}", "long", sizeof(long), long.MinValue, long.MaxValue);
+WriteLine("{0, -8} {1, 1} {2, 30} {3, 30}", "ulong", sizeof(ulong), ulong.MinValue, ulong.MaxValue);
+WriteLine("{0, -8} {1, 1} {2, 30} {3, 30}", "float", sizeof(float), float.MinValue, float.MaxValue);
+WriteLine("{0, -8} {1, 1} {2, 30} {3, 30}", "double", sizeof(double), double.MinValue, double.MaxValue);
+WriteLine("{0, -8} {1, 1} {2, 30} {3, 30}", "decimal", sizeof(decimal), decimal.MinValue, decimal.MaxValue);
