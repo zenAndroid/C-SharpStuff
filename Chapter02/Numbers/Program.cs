ea9616d63675fbc8f47e10771030962df9f1964c
zenAndroid <21260658+zenAndroid@users.noreply.github.com>
2022-11-07 11:03:28 -0500

    Numbers ...

---
+++ Chapter02/Numbers/Program.cs
@@ -0,0 +1,16 @@
+﻿// three variables that store the number 2 million
+int decimalNotation = 2_000_000;
+int binaryNotation = 0b_0001_1110_1000_0100_1000_0000;
+int hexadecimalNotation = 0x_001E_8480;
+// check the three variables have the same value
+// both statements output true
+Console.WriteLine($"{decimalNotation == binaryNotation}");
+Console.WriteLine(
+$"{decimalNotation == hexadecimalNotation}");
+
+
+/*
+ * Output, unsurprinsingly, is:
+True
+True
+*/
