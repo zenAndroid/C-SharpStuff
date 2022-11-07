db190bb9bae847c9530a0b4b6ff5947f573a4c4c
zenAndroid <21260658+zenAndroid@users.noreply.github.com>
2022-11-07 14:32:01 -0500

    eh, color stuff

--- Chapter02/Arguments/Program.cs
+++ Chapter02/Arguments/Program.cs
@@ -17,3 +17,18 @@ second-arg
 third:arg
 fourth arg
 */
+
+if (args.Length < 3)
+{
+    WriteLine("You must specify two colors and cursor size, e.g.");
+    WriteLine("dotnet run red yellow 50");
+    return; // stop running
+}
+ForegroundColor = (ConsoleColor)Enum.Parse(
+enumType: typeof(ConsoleColor),
+value: args[0],
+ignoreCase: true);
+BackgroundColor = (ConsoleColor)Enum.Parse(
+enumType: typeof(ConsoleColor), value: args[1],
+ignoreCase: true);
+CursorSize = int.Parse(args[2]);
