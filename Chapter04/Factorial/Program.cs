b0a732266c65a8c8c3e6da61230b63088a97517f
zenAndroid <21260658+zenAndroid@users.noreply.github.com>
2022-11-08 21:49:34 -0500

    ...

--- Chapter04/Factorial/Program.cs
+++ Chapter04/Factorial/Program.cs
@@ -1,61 +1,76 @@
 ﻿
-
-
-static string CardinalToOrdinal(int number)
+class Program
 {
-    switch (number)
+    /// <summary>
+    /// Ah shit
+    /// </summary>
+    /// <param name="number">A number</param>
+    /// <returns></returns>
+    static string CardinalToOrdinal(int number)
     {
-        case 11: // special cases for 11th to 13th
-        case 12:
-        case 13:
-            return $"{number}th";
-        default:
-            int lastDigit = number % 10;
-            string suffix = lastDigit switch
+        switch (number)
+        {
+            case 11: // special cases for 11th to 13th
+            case 12:
+            case 13:
+                return $"{number}th";
+            default:
+                int lastDigit = number % 10;
+                string suffix = lastDigit switch
+                {
+                    1 => "st",
+                    2 => "nd",
+                    3 => "rd",
+                    _ => "th"
+                };
+                return $"{number}{suffix}";
+        }
+    }
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(Factorial(15));
+        Console.WriteLine(CardinalToOrdinal(45));
+        RunFactorial();
+    }
+    /// <summary>
+    /// Calculates factorials
+    /// </summary>
+    /// <param name="number">The number whose factorial we want</param>
+    /// <returns>Ok, so apparently this auto-generated XML doc business only woorks for methods and not top-level functions ...
+    /// Jesus christ that would have been good to know <b>BEFORE</b> wasting time trying to figure the old one out, so much for ,, ugh whatever</returns>
+    static ulong Factorial(ulong number)
+    {
+        if (number < 0)
+        {
+            return 0;
+        }
+        else if (number == 0)
+        {
+            return 1;
+        }
+        else
+        {
+            checked
             {
-                1 => "st",
-                2 => "nd",
-                3 => "rd",
-                _ => "th"
-            };
-            return $"{number}{suffix}";
+                return number * Factorial(number - 1);
+            }
+        }
     }
-}
-
-
-static ulong Factorial(ulong number)
-{
-    if (number < 0)
+    /// <summary>
+    /// <i>Sigh</i>
+    /// </summary>
+    static void RunFactorial()
     {
-        return 0;
-    } else if (number == 0)
-    {
-        return 1;
-    } else
-    {
-        checked
+        for (ulong i = 1; i < 25; i++)
         {
-            return number * Factorial(number - 1); 
+            try
+            {
+                Console.WriteLine($"{i}! = {Factorial(i):N0}");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine($"{i}! is too big for a `ulong`!");
+            }
         }
     }
 }
-
-Console.WriteLine(Factorial(15));
-Console.WriteLine(CardinalToOrdinal(45));
-
-static void RunFactorial()
-{
-    for (ulong i = 1; i < 25; i++)
-    {
-        try
-        {
-            Console.WriteLine($"{i}! = {Factorial(i):N0}");
-        }
-        catch (OverflowException)
-        {
-            Console.WriteLine($"{i}! is too big for a `ulong`!");
-        }
-    }
-}
-
-RunFactorial();
