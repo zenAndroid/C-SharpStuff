641f4e9265588be893f9dc748d68b52c4d9fdbf5
zenAndroid <21260658+zenAndroid@users.noreply.github.com>
2022-11-13 19:20:23 -0500

    Hashtables, yay.

--- Chapter06/PeopleApp/Program.cs
+++ Chapter06/PeopleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Packt.Shared;
+using System.Linq;
 using static System.Console;
 
 Person harry = new() { Name = "Harry" };
@@ -32,3 +33,22 @@ harry.Poke();
 harry.Poke();
 harry.Poke();
 harry.Poke();
+
+Console.WriteLine("----------------------------------");
+
+System.Collections.Hashtable lookupObject = new();
+
+lookupObject.Add(key: 'j', value: "Alpha");
+lookupObject.Add(key: "but", value: "Science");
+lookupObject.Add(key: 3, value: "Gamma");
+lookupObject.Add(key: harry, value: "Delta");
+
+var key = "but"; // lookup the value that has 2 as its key
+WriteLine(format: "Key `{0}` has value: {1}",
+arg0: key,
+arg1: lookupObject[key]);
+
+// lookup the value that has harry as its key
+WriteLine(format: "Key {0} has value: {1}",
+arg0: harry,
+arg1: lookupObject[harry]);
